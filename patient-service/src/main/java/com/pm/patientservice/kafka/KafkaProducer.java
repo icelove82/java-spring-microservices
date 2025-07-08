be2adb5e61413951cbd72a1a12a67c1d747aa2ba
YUN <yunmyeonghun@gmail.com>
2025-07-08 17:31:53 +0900

    Refactor `patient-service` with `lombok`, formatting, and boilerplate removal
    - Added `lombok` annotations (`@Data`, `@Slf4j`, `@RequiredArgsConstructor`) to reduce boilerplate in models, services, and Kafka producer.
    - Removed redundant getters, setters, and constructors in `Patient` and other classes.
    - Enhanced code formatting for improved consistency and readability.

--- patient-service/src/main/java/com/pm/patientservice/kafka/KafkaProducer.java
+++ patient-service/src/main/java/com/pm/patientservice/kafka/KafkaProducer.java
@@ -1,34 +1,31 @@
 package com.pm.patientservice.kafka;
+
 import com.pm.patientservice.model.Patient;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import patient.events.PatientEvent;
 
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class KafkaProducer {
 
-  private static final Logger log = LoggerFactory.getLogger(
-      KafkaProducer.class);
-  private final KafkaTemplate<String, byte[]> kafkaTemplate;
+    private final KafkaTemplate<String, byte[]> kafkaTemplate;
 
-  public KafkaProducer(KafkaTemplate<String, byte[]> kafkaTemplate) {
-    this.kafkaTemplate = kafkaTemplate;
-  }
+    public void sendEvent(Patient patient) {
+        PatientEvent event = PatientEvent.newBuilder()
+                .setPatientId(patient.getId().toString())
+                .setName(patient.getName())
+                .setEmail(patient.getEmail())
+                .setEventType("PATIENT_CREATED")
+                .build();
 
-  public void sendEvent(Patient patient) {
-    PatientEvent event = PatientEvent.newBuilder()
-        .setPatientId(patient.getId().toString())
-        .setName(patient.getName())
-        .setEmail(patient.getEmail())
-        .setEventType("PATIENT_CREATED")
-        .build();
-
-    try {
-      kafkaTemplate.send("patient", event.toByteArray());
-    } catch (Exception e) {
-      log.error("Error sending PatientCreated event: {}", event);
+        try {
+            kafkaTemplate.send("patient", event.toByteArray());
+        } catch (Exception e) {
+            log.error("Error sending PatientCreated event: {}", event);
+        }
     }
-  }
 }
